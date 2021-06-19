fa8161c3f5f17e31e5b16e42b10f33fecd1d9a3d
boorlashivaprasad <84766378+boorlashivaprasad@users.noreply.github.com>
2021-06-19 19:28:53 +0530

    test_max
    solution for testing maximum number

--- solns.c
+++ solns.c
@@ -1,2 +1,18 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int a[], int n)
+{
+    int sol = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > sol)
+            sol = a[i];
+    }
+    return sol;
+}
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5, 6, 7};
+    int sol = max(a, 7);
+    printf("%d\n",sol);
+}
